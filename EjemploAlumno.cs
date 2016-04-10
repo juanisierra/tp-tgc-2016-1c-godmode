bdfcc49b70e7e08e40a1b4ffbf529501fd024827
Juan Ignacio Sierra <juanignaciosierra96@gmail.com>
2016-04-10 11:53:26 -0300

    Subo Primera version
    Movi la posicion de inicio a la mitad y deje prendido movimiento en y
    para debuggear

---
+++ EjemploAlumno.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TgcViewer.Example;
+using TgcViewer;
+using Microsoft.DirectX.Direct3D;
+using System.Drawing;
+using Microsoft.DirectX;
+using TgcViewer.Utils.Modifiers;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.TgcGeometry;
+namespace AlumnoEjemplos.GODMODE
+{
+    /// <summary>
+    /// Ejemplo del alumno
+    /// </summary>
+    public class EjemploAlumno : TgcExample
+    {   ///Configuraciones del Tgc-Viewer
+        ///Configuraciones del Ejemplo
+        /// <summary>
+        /// Categoría a la que pertenece el ejemplo.
+        /// Influye en donde se va a haber en el árbol de la derecha de la pantalla.
+        /// </summary>
+        public override string getCategory()
+        {
+            return "AlumnoEjemplos";
+        }
+
+        /// <summary>
+        /// Completar nombre del grupo en formato Grupo NN
+        /// </summary>
+        public override string getName()
+        {
+            return "Grupo GODMODE";
+        }
+
+        /// <summary>
+        /// Completar con la descripción del TP
+        /// </summary>
+        public override string getDescription()
+        {
+            return "Survival Horror";
+        }
+
+        TgcScene tgcScene; // Crea la escena
+        /// <summary>
+        /// Método que se llama una sola vez,  al principio cuando se ejecuta el ejemplo.
+        /// Escribir aquí todo el código de inicialización: cargar modelos, texturas, modifiers, uservars, etc.
+        /// Borrar todo lo que no haga falta
+        /// </summary>
+        public override void init()
+        {
+            //GuiController.Instance: acceso principal a todas las herramientas del Framework
+
+            //Device de DirectX para crear primitivas
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+
+            //Carpeta de archivos Media del alumno
+            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosDir;
+
+            TgcSceneLoader loader = new TgcSceneLoader(); //TgcsceneLoader para cargar el escenario
+            tgcScene = loader.loadSceneFromFile(            //Carga el escenario
+                alumnoMediaFolder + "GODMODE\\Media\\mapa.xml",
+                alumnoMediaFolder + "GODMODE\\Media\\");
+
+
+            ///////////////CONFIGURAR CAMARA PRIMERA PERSONA//////////////////
+            //Camara en primera persona, tipo videojuego FPS
+            //Solo puede haber una camara habilitada a la vez. Al habilitar la camara FPS se deshabilita la camara rotacional
+            //Por default la camara FPS viene desactivada
+            GuiController.Instance.FpsCamera.Enable = true;
+            //Configurar posicion y hacia donde se mira
+            GuiController.Instance.FpsCamera.setCamera(new Vector3(3261.925f, 65, 2519.217f), new Vector3(0, 0, 0));
+            GuiController.Instance.FpsCamera.Velocity = new Vector3(300f, 100f, 300f); ///Cambiamos Velocidad de la camara y anulamos y
+        }
+        /// <summary>
+        /// Método que se llama cada vez que hay que refrescar la pantalla.
+        /// Escribir aquí todo el código referido al renderizado.
+        /// Borrar todo lo que no haga falta
+        /// </summary>
+        // <param name="elapsedTime">Tiempo en segundos transcurridos desde el último frame</param>
+        public override void render(float elapsedTime)
+        {
+            
+            tgcScene.renderAll(); //Renderiza la escena del TGCSceneLoader
+            //Device de DirectX para renderizar
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+
+
+            
+
+            ///////////////INPUT//////////////////
+            //conviene deshabilitar ambas camaras para que no haya interferencia
+
+            //Capturar Input teclado 
+            if (GuiController.Instance.D3dInput.keyPressed(Microsoft.DirectX.DirectInput.Key.F))
+            {
+                //Tecla F apretada
+            }
+
+            //Capturar Input Mouse
+            if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT))
+            {
+                //Boton izq apretado
+            }
+
+        }
+
+        /// <summary>
+        /// Método que se llama cuando termina la ejecución del ejemplo.
+        /// Hacer dispose() de todos los objetos creados.
+        /// </summary>
+        public override void close()
+        {
+            tgcScene.disposeAll();
+        }
+
+    }
+}
